6daa3e0d3381ed15ddb3dc605898d468e068e53b
OpenShift guest <andy.4705@gmail.com>
2013-04-10 07:16:32 -0400

    separated out socket.js from vote.js. beginning of hooks

--- vote.js
+++ vote.js
@@ -1,45 +1,24 @@
-var socket = null;
 
-var can = document.getElementById("canvas");
-var ctx = can.getContext("2d");
-var colours = new Array('#FF0000','0000FF','#00FF00','FF00FF','#FFFF00','00FFFF');
-    
+function vote_onconnect(){
 
-function init(){
-    connect();    
 }
 
-function connect() {
-    log('Connecting to local server...');
-    if (socket == null) {
-        socket = io.connect(null,{'auto connect': false});
-        socket.on('connect', function () {
-             status('Connected');
-        });
-        
-        socket.on('message', function (data) {
-            
-            try {
-                var json = JSON.parse(data);
-            } catch (e) {
-                console.log('This doesn\'t look like a valid JSON: ', data);
-                return;
-            }
-            
-            if (json.type=='message'){
-                //log(json.type);
-                log(json.data);
-            }
-            
-            if (json.type=='votes'){
-                updateVotes(json.data);            
-            }
-            
-        });
+function vote_onmessage(json){
+ 
+    if (json.type=='message'){
+        //log(json.type);
+        log(json.data);
     }
-    socket.socket.connect();
+    
+    if (json.type=='votes'){
+        updateVotes(json.data);            
+    }
+
+
 }
 
+
+// send message from text box thing
 function send() {
     if (socket && socket.socket.connected) {
         socket.send(JSON.stringify({ type:'message', data:document.getElementById('text').value}));
@@ -48,6 +27,11 @@ function send() {
         log('Not connected.');
     }
 }
+
+
+var can = document.getElementById("canvas");
+var ctx = can.getContext("2d");
+var colours = new Array('#FF0000','0000FF','#00FF00','FF00FF','#FFFF00','00FFFF');
 
 function updateVotes(data){
     
@@ -87,14 +71,3 @@ function vote(option) {
     }
 }
 
-function update() {
-    if (socket && socket.socket && socket.socket.transport) {
-        document.getElementById('sessionId').textContent = socket.socket.transport.sessid;
-        document.getElementById('transport').textContent = socket.socket.transport.name;
-    } else {
-        document.getElementById('sessionId').textContent = '-';
-        document.getElementById('transport').textContent = '-';
-    }
-}
-
-setInterval(update, 10);
